6192543521bd54ecba45b7538e0a45004844caa0
suyash1133 <92840664+suyash1133@users.noreply.github.com>
2021-11-23 09:45:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";
+}
+
+function apply_tint()
+{
+    tint_color=document.getElementById("color_name").value;
+}
+
+function take_snapshot()
+{
+    save("student_name.png");
+}
+
+function draw()
+{
+    image(video,0,0,640,480);
+    tint(tint_color);
+}
